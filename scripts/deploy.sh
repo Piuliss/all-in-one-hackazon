69ce97d3e20a06b13e13af92f268b65b1a1bd4cf
Raul Benitez Netto <raulbeni@gmail.com>
2024-04-20 09:28:43 -0400

    minor change in deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
+echo "Removing fiuni_web service"
 # Eliminar el servicio y stack existente
 docker service rm fiuni_web
-
+echo "Waiting for 20 seconds ..."
 # Esperar unos segundos para que se eliminen correctamente
 sleep 20
-
+echo "Deploy stack again"
 # Implementar el stack en Swarm
 docker stack deploy --compose-file ../docker-compose.yml fiuni
